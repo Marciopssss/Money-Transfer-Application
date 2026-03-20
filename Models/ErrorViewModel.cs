66daaaf4546b8dfde1a10d9d9029a8186e69b5f2
Marciopssss <marceldahdah278@icloud.com>
2026-03-20 15:00:11 +0200

    Initial commit + NuGet setups + appdb

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Money_Trasnfer_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
